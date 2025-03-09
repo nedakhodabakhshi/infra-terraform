36cf7953e9f45522ad99b1d87686897c37795b3b
Neda Khodabakhshi <khodabakhshi.neda@gmail.com>
2025-03-09 22:26:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,49 @@ terraform {
 
   backend "azurerm" {
     resource_group_name   = "my-rg"
-    storage_account_name  = "mytorage"
+    storage_account_name  = "myterraformstorage"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
 }
 
-resource "azurerm_resource_group" "vprofil
+resource "azurerm_resource_group" "rg" {
+  name     = "my-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "my-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "backend_subnet" {
+  name                 = "backend-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "my-aks-cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "myaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_D2s_v3"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
