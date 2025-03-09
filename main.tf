a8709e535c4f66e3ddb73060b4c74b3de1886a1c
Neda Khodabakhshi <khodabakhshi.neda@gmail.com>
2025-03-09 22:04:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,21 @@
-# resource "azurerm_resource_group" "my_rg" {
-  # name     = var.resource_group_name
-  #location = var.location
-#}
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name   = "vprofile-rg"
+    storage_account_name  = "vprofilestorage"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "vprofil
